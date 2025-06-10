034b3663a48326e167ef99c1ff6c4591359dfce7
SYSTEM-OTMX <gtainside222@gmail.com>
2025-06-10 08:47:34 -0600

    AVANCE DEL CURSO

---
+++ projects/00-hola-mundo/src/main.jsx
@@ -0,0 +1,11 @@
+import { Fragment, StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+
+createRoot(document.getElementById('root')).render(
+  <frameElement>
+    <button>Hola Mundo</button>
+    <button>Hola Mundo</button>
+  </frameElement>
+  
+)
